25ecf5ea35814d5df363c68e392be1e876ae1a62
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2016-11-26 14:06:48 +0200

    Removed done TODO comments

--- src/units/Processor.java
+++ src/units/Processor.java
@@ -18,33 +18,31 @@ public class Processor {
 	 * 
 	 * Missing
 	 * =======
-	 * 1. Initialization of constructor
-	 * 2. Execute instructions
+	 * 1. Execute instructions
 	 * 2. Write result instructions
 	 * 3. Commit instructions
 	 */
 	private static final byte VALID = -1;
 	private ReservationStation[] reservationStations;	
-	private ReorderBuffer ROB;							//TODO initialize with size from input
+	private ReorderBuffer ROB;							
 	private short[] registerFile;						
 	private short[] registerStatus;						
 	private MemoryHandler memoryUnit;					
-	private Queue<Short> instructionQueue;				//TODO initialize with size from input
+	private Queue<Short> instructionQueue;				
 	private int instructionQueueMaxSize;
-	private int pipelineWidth;							//TODO input
+	private int pipelineWidth;							
 	private int[] firstReservationStation;
-	private int[] countReservationStation; 				//TODO initialize with size 5 and values from input
+	private int[] countReservationStation; 				
 
 	public Processor(){
-		//TODO: initialization
 		registerFile = new short[8];
 		registerStatus = new short[8];
 		Arrays.fill(registerStatus, (short) VALID);
 		countReservationStation = new int[5];
-		
-		 prepareReservationStations();
+
+		prepareReservationStations();
 	}
-	
+
 	private void prepareReservationStations(){
 		int totalRS = 0;
 		firstReservationStation = new int[5];
@@ -59,7 +57,7 @@ public class Processor {
 			}
 		}	
 	}
-	
+
 	private void issueInstructions(){
 		mainLoop: for(int i = 0; i < pipelineWidth && !instructionQueue.isEmpty() && !getROB().isFull(); ++i){
 			short currentInstruction = instructionQueue.peek();
@@ -74,25 +72,25 @@ public class Processor {
 			break;
 		}
 	}
-	
+
 	private void executeInstructions(){
 		//TODO if instruction finished issuing
 		for(ReservationStation rs: reservationStations){
 			rs.executeInstruction();
 		}
 	}
-	
+
 	private void writeResultInstructions(){
 		for(ReservationStation rs: reservationStations){
 			//TODO if rs finished execution
 			rs.writeInstruction();
 		}
 	}
-	
+
 	private void commitInstructions(){
 		ROB.commit();
 	}
-	
+
 	public void clear() {
 		Arrays.fill(registerStatus, VALID);
 		ROB.clear();
@@ -100,7 +98,7 @@ public class Processor {
 			rs.clearBusy();
 		}
 	}
-	
+
 	public short getRegisterStatus(byte register) {
 		return registerStatus[register];
 	}
@@ -112,19 +110,19 @@ public class Processor {
 	public ReorderBuffer getROB() {
 		return ROB;
 	}
-	
+
 	public short getRegisterValue(byte register) {
 		return registerFile[register];
 	}
-	
+
 	public MemoryHandler getMemoryUnit() {
 		return this.memoryUnit;
 	}
-	
+
 	public void setMemoryUnit(MemoryHandler memoryUnit) {
 		this.memoryUnit = memoryUnit;
 	}
-	
+
 	public int[] getCountReservationStation() {
 		return countReservationStation;
 	}
@@ -140,13 +138,13 @@ public class Processor {
 	public void setPipelineWidth(int pipelineWidth) {
 		this.pipelineWidth = pipelineWidth;
 	}
-	
+
 	public void setRegisterValue(byte register, short value) {
 		registerFile[register] = value;
 	}
-	
+
 	public ReservationStation[] getReservationStations(){
 		return reservationStations;
 	}
-	
+
 }
